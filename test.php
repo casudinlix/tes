e9d082246c32a20642411f67618dbbf3f529d6bb
root <root@Duddin>
2017-09-15 23:16:18 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test {
+
+  public function method()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+}
